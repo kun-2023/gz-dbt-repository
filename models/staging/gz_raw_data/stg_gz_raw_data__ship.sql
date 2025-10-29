ce21427810f18601f7f70488e0c804aed7ba828e
Kun Bi <kunbi3456@gmail.com>
2025-10-29 19:52:55 +0000

    cleaned three generated queries

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        -- shipping_fee_1,
+        logcost,
+        cast(ship_cost,float64) as ship_cost
+        shipping_fee-shipping_fee_1 as fee_differences
+
+    from source
+    where shipping_fee<>shipping_fee_1
+
+)
+
+select * from renamed
