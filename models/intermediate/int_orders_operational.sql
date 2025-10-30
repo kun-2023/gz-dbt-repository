4cfe34f8112cd35d6cdf6ba41d2534c3eb22c438
Kun Bi <kunbi3456@gmail.com>
2025-10-30 15:48:07 +0000

    build int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,10 +3,9 @@ o.orders_id,
 o.date_date,
 o.margin,
 s.shipping_fee,
-s.log_cost,
+s.logcost,
 s.ship_cost,
-cast(ship_cost as float64) as ship_cost,
-round(o.margin+s.shipping_fee-s.log_cost-cast(s.ship_cost as float64),2) as operational_margin
+round(o.margin+s.shipping_fee-s.logcost-s.ship_cost,2) as operational_margin
 from 
 {{ref("int_orders_margin")}} o
 left join
