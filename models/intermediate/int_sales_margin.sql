cddc3ac7169aad8f58a2ae4ed46659c1786ada1c
Kun Bi <kunbi3456@gmail.com>
2025-10-30 15:16:28 +0000

    build int_sales_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,12 @@
-select *
+select 
+products_id,
+date_date,
+orders_id,
+revenue,
+quantity,
+purchase_price,
+round(s.quantity*p.purchase_price,2) as purchase_cost,
+round(s.revenue-s.quantity*p.purchase_price,2) as margin
 from {{ref("stg_gz_raw_data__sales")}} s
-join {{ref("stg_gz_raw_data__product")}} p
+left join {{ref("stg_gz_raw_data__product")}} p
 using (products_id)
