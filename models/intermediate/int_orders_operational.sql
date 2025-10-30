b040f28e15d9e4b4f9a6a2d84a537e1a0cf2c5b3
Kun Bi <kunbi3456@gmail.com>
2025-10-30 15:36:19 +0000

    build intermediate table int_orders_margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,15 @@
+select
+o.orders_id,
+o.date_date,
+o.margin,
+s.shipping_fee,
+s.log_cost,
+s.ship_cost,
+cast(ship_cost as float64) as ship_cost,
+round(o.margin+s.shipping_fee-s.log_cost-cast(s.ship_cost as float64) as Operational_margin,2)
+from 
+{{ref("int_orders_margin")}} o
+left join
+{{ref("stg_gz_raw_data__ship")}} s
+using(orders_id)
+order by orders_id desc
