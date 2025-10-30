931edf11631135b42f689521744c59ae0c0f3296
Kun Bi <kunbi3456@gmail.com>
2025-10-30 16:42:50 +0000

    finance_days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,17 +1,17 @@
 with a as
 (select 
 s.date_date,-- Date
-count(distinct s.orders_id) as nb_transactions -- Total number of transactions
-sum(s.revenue) as revenue-- Total revenue
-sum(s.revenue)/count(distinct s.orders_id) as avg_basket-- Average Basket
+count(distinct s.orders_id) as nb_transactions, -- Total number of transactions
+sum(s.revenue) as revenue,-- Total revenue
+sum(s.revenue)/count(distinct s.orders_id) as avg_basket,-- Average Basket
 sum(s.purchase_cost) as purchase_cost --Total Purchase Cost
 from {{ref("int_sales_margin")}} s
 group by date_date),
 b as(select 
 m.date_date,
 sum(o.operational_margin) as Operational_margin,-- Operational Margin
-sum(o.shipping_fee) as shipping_cost-- Total shipping fees
-sum(logcost) as log_cost-- Total log costs
+sum(o.shipping_fee) as shipping_cost,-- Total shipping fees
+sum(logcost) as log_cost,-- Total log costs
 sum(m.quantity) as total_quantity-- Total quantity of products sold.
 from 
 {{ref("int_orders_margin")}} m
