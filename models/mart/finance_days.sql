7bb0b846d6e2c7b378d678999fbead46711a4c08
Kun Bi <kunbi3456@gmail.com>
2025-10-30 17:02:29 +0000

    finance_days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -5,12 +5,13 @@ with a as(
     round(sum(margin),0) as margin,
     round(sum(operational_margin),0) as operational_margin,
     round(sum(purchase_cost),0) as purchase_cost,
-    round(sum(shipping_cost)) as shipping_cost,
+    round(sum(ship_cost)) as ship_cost,
     round(sum(logcost)) as log_cost,
-    round(sum(ship_cost),0) as ship_cost,
+    round(sum(shipping_fee)) as shipping_fee,
     sum(quantity) as quantity
     from {{ref("int_orders_operational")}}
     group by date_date)
 select *,
-revenue/nullif(nb_transactions) as avg_basket
-from asorder by date_date desc;
+revenue/nullif(nb_transactions,0) as avg_basket
+from a
+order by date_date desc;
